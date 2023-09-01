4615528860e0e3080d01a9f92bdce457dcc10305
Michael Ivanov <ivanovmihael98@gmail.com>
2023-09-01 11:48:18 +0300

    Authentication controller

---
+++ shoppinglist/src/main/java/com/example/shoppinglist/controller/AuthenticationController.java
@@ -0,0 +1,24 @@
+package com.example.shoppinglist.controller;
+
+import com.example.shoppinglist.dto.AuthenticationRequest;
+import com.example.shoppinglist.dto.AuthenticationResponse;
+import com.example.shoppinglist.service.AuthenticationService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users/login")
+public class AuthenticationController {
+
+    private AuthenticationService authenticationService;
+
+    @PostMapping
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.login(request));
+    }
+}
--- shoppinglist/src/main/java/com/example/shoppinglist/service/JwtService.java
+++ shoppinglist/src/main/java/com/example/shoppinglist/service/JwtService.java
@@ -1,8 +1,10 @@
 package com.example.shoppinglist.service;
 
 import com.example.shoppinglist.config.JwtConfig;
+import com.example.shoppinglist.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
@@ -26,6 +28,16 @@ public class JwtService {
     public boolean isValidToken(UserDetails user, String token) {
         String username = extractUsername(token);
         return username.equals(user.getUsername()) && !isExpired(token);
+    }
+
+    public String generateToken(User user) {
+        return Jwts
+            .builder()
+            .setSubject(user.getUsername())
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 24 * 60))
+            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+            .compact();
     }
 
     private Claims extractAllClaims(String token) {
